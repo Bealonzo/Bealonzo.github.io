8df76f61b3f1fa0b42663c76bcfec329ea5e45ec
Bealonzo <45976585+Bealonzo@users.noreply.github.com>
2019-09-07 18:35:00 -0400

    clean-up

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
-
+import avatar from '../assets/myAvatar.png'
 
 class Landing extends Component {
   render() {
@@ -9,7 +9,7 @@ class Landing extends Component {
         <Grid className="landing-grid">
           <Cell col={12}>
             <img
-              src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+              src={avatar}
               alt="avatar"
               className="avatar-img"
               />
@@ -19,29 +19,21 @@ class Landing extends Component {
 
             <hr/>
 
-          <p>HTML/CSS | Bootstrap | JavaScript | React | React Native | NodeJS | Express | MongoDB</p>
+          <p>HTML/CSS | Bootstrap | JavaScript | React | React Native | NodeJS | Express | MySQL</p>
 
         <div className="social-links">
 
           {/* LinkedIn */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+          <a href="https://www.linkedin.com/in/brian-alonzo-aa613a110/" rel="noopener noreferrer" target="_blank">
             <i className="fa fa-linkedin-square" aria-hidden="true" />
           </a>
 
           {/* Github */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+          <a href="https://github.com/Bealonzo" rel="noopener noreferrer" target="_blank">
             <i className="fa fa-github-square" aria-hidden="true" />
           </a>
 
-          {/* Freecodecamp */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-free-code-camp" aria-hidden="true" />
-          </a>
-
-          {/* Youtube */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-youtube-square" aria-hidden="true" />
-          </a>
+         
 
         </div>
             </div>
--- src/components/resume.js
+++ src/components/resume.js
@@ -3,6 +3,7 @@ import { Grid, Cell } from 'react-mdl';
 import Education from './education';
 import Experience from './experience';
 import Skills from './skills';
+import avatar from '../assets/myAvatar.png'
 
 
 class Resume extends Component {
@@ -13,23 +14,21 @@ class Resume extends Component {
           <Cell col={4}>
             <div style={{textAlign: 'center'}}>
               <img
-                src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+                src={avatar}
                 alt="avatar"
                 style={{height: '200px'}}
                  />
             </div>
 
-            <h2 style={{paddingTop: '2em'}}>Paul Hanna</h2>
-            <h4 style={{color: 'grey'}}>Programmer</h4>
+            <h2 style={{paddingTop: '2em'}}>Brian Alonzo</h2>
+            <h4 style={{color: 'grey'}}>Web developer</h4>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
-            <h5>Address</h5>
-            <p>1 Hacker Way Menlo Park, 94025</p>
             <h5>Phone</h5>
-            <p>(123) 456-7890</p>
+            <p>(845) 505-7680</p>
             <h5>Email</h5>
-            <p>someone@example.com</p>
+            <p>Alonzo.Briane@gmail.com</p>
             <h5>Web</h5>
             <p>mywebsite.com</p>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
@@ -53,7 +52,7 @@ class Resume extends Component {
                   />
                 <hr style={{borderTop: '3px solid #e22947'}} />
 
-              <h2>Experience</h2>
+              <h2>Key Project</h2>
 
             <Experience
               startYear={2009}
@@ -72,19 +71,19 @@ class Resume extends Component {
               <h2>Skills</h2>
               <Skills
                 skill="javascript"
-                progress={100}
+                progress={75}
                 />
                 <Skills
                   skill="HTML/CSS"
-                  progress={80}
+                  progress={90}
                   />
                   <Skills
                     skill="NodeJS"
-                    progress={50}
+                    progress={40}
                     />
                     <Skills
                       skill="React"
-                      progress={25}
+                      progress={45}
                       />
 
 
