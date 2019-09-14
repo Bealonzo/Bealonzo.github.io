681aff4474e06ce20ee55cf012fa2372a73f053f
Bealonzo <45976585+Bealonzo@users.noreply.github.com>
2019-09-13 21:33:50 -0400

    Updates

---
+++ precache-manifest.88446c5a6afdf59f63ed090d390681c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5e03fdd808d70bbf46f8bc35c32e08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12e664c77657535c826d",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "5380df6052913d039ce5",
+    "url": "/static/css/main.00de0269.chunk.css"
+  },
+  {
+    "revision": "12e664c77657535c826d",
+    "url": "/static/js/2.05ec16a8.chunk.js"
+  },
+  {
+    "revision": "5380df6052913d039ce5",
+    "url": "/static/js/main.de7fabf1.chunk.js"
+  },
+  {
+    "revision": "b63f2f39c40843be3b60",
+    "url": "/static/js/runtime~main.1142cf62.js"
+  },
+  {
+    "revision": "7d0be9a67d0ae24f9fbb7c3fbb42c9d1",
+    "url": "/static/media/myAvatar.7d0be9a6.png"
+  }
+]);
